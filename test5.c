937e753de49cc7e93a199c4836d1058500e516d6
mightysai1997 <110396758+mightysai1997@users.noreply.github.com>
2024-04-19 14:21:57 +0530

    Update test5.c

--- test5.c
+++ test5.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 #include <libxml/parser.h>
-#include <libxml/xmlschemas.h>
 
 int main() {
     xmlDocPtr doc;
-    xmlSchemaPtr schema;
-    xmlSchemaParserCtxtPtr schemaParserCtxt;
-    xmlSchemaValidCtxtPtr schemaValidCtxt;
     
     // User-supplied XML input (replace this with actual input)
-    const char* xmlData = "<!DOCTYPE foo [<!ENTITY xxe SYSTEM \"file:///etc/passwd\">]><root>&xxe;</root>";
-    const char* xsdData = "<xs:schema xmlns:xs=\"http://www.w3.org/2001/XMLSchema\"><xs:element name=\"root\" type=\"xs:string\"/></xs:schema>";
+    const char* xmlData = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+                          "<!DOCTYPE foo [\n"
+                          "<!ENTITY xxe SYSTEM \"file:///etc/passwd\">\n"
+                          "]>\n"
+                          "<root>&xxe;</root>";
     
     // Parse XML document
     doc = xmlReadMemory(xmlData, strlen(xmlData), NULL, NULL, 0);
@@ -19,49 +18,9 @@ int main() {
         return 1;
     }
     
-    // Create XML schema parser context
-    schemaParserCtxt = xmlSchemaNewMemParserCtxt(xsdData, strlen(xsdData));
-    if (schemaParserCtxt == NULL) {
-        fprintf(stderr, "Error: Failed to create XML schema parser context.\n");
-        xmlFreeDoc(doc);
-        return 1;
-    }
-    
-    // Parse XML schema
-    schema = xmlSchemaParse(schemaParserCtxt);
-    if (schema == NULL) {
-        fprintf(stderr, "Error: Failed to parse XML schema.\n");
-        xmlSchemaFreeParserCtxt(schemaParserCtxt);
-        xmlFreeDoc(doc);
-        return 1;
-    }
-    
-    // Create XML schema validation context
-    schemaValidCtxt = xmlSchemaNewValidCtxt(schema);
-    if (schemaValidCtxt == NULL) {
-        fprintf(stderr, "Error: Failed to create XML schema validation context.\n");
-        xmlSchemaFree(schema);
-        xmlSchemaFreeParserCtxt(schemaParserCtxt);
-        xmlFreeDoc(doc);
-        return 1;
-    }
-    
-    // Validate XML document against schema
-    if (xmlSchemaValidateDoc(schemaValidCtxt, doc) != 0) {
-        fprintf(stderr, "Error: XML document failed schema validation.\n");
-        xmlSchemaFreeValidCtxt(schemaValidCtxt);
-        xmlSchemaFree(schema);
-        xmlSchemaFreeParserCtxt(schemaParserCtxt);
-        xmlFreeDoc(doc);
-        return 1;
-    }
-    
     // Process XML data
     
-    // Free resources
-    xmlSchemaFreeValidCtxt(schemaValidCtxt);
-    xmlSchemaFree(schema);
-    xmlSchemaFreeParserCtxt(schemaParserCtxt);
+    // Free the document
     xmlFreeDoc(doc);
     
     return 0;
