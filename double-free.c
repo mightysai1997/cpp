0b06467ca20bf89cfe2703d417abf854276ed695
mightysai1997 <110396758+mightysai1997@users.noreply.github.com>
2024-04-29 16:26:31 +0530

    Update double-free.c

--- double-free.c
+++ double-free.c
@@ -1,3 +1,9 @@
+/*
+Sample code for vulnerable type: Double free
+CWE : CWE-415
+Description : The below-provided code, Potential double call to free. ptr may have already been freed.
+*/
+
 #include <stdlib.h>
 #define SIZE 16
 int main(){
